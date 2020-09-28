8a2c587413fa589b536a3ee1b5527baf3357c7f5
ak1215 <60421957+ak1215@users.noreply.github.com>
2020-09-28 16:08:36 +0200

    ArrayList of movies

--- src/ch/tbz/m226a/cinema/MovieList.java
+++ src/ch/tbz/m226a/cinema/MovieList.java
@@ -3,15 +3,21 @@ package ch.tbz.m226a.cinema;
 import java.util.ArrayList;
 
 public class MovieList {
-    ArrayList<Movie> movieArrayList = new ArrayList<>();
+    ArrayList<Movie> afterTruth = new ArrayList<>();
+    ArrayList<Movie> braveMaedchentunDasNicht = new ArrayList<>();
+    ArrayList<Movie> dieSchoenstenJahreMeinesLebens = new ArrayList<>();
+    ArrayList<Movie> iStillBelieve = new ArrayList<>();
+    ArrayList<Movie> jeanSerbegAgainstAllEnemies = new ArrayList<>();
+    ArrayList<Movie> justMercy = new ArrayList<>();
+
 
     public void addMovie(){
-        movieArrayList.add("AFTER TRUTH");
-        movieArrayList.add("BRAVE MÄDCHEN TUN DAS NICHT");
-        movieArrayList.add("DIE SCHÖNSTEN JAHRE MEINES LEBENS");
-        movieArrayList.add("I STILL BELIEVE");
-        movieArrayList.add("JEAN SEBREG - AGAINST ALL ENEMIES");
-        movieArrayList.add("JUST MERCY");
+        afterTruth.add("AFTER TRUTH");
+        braveMaedchentunDasNicht.add("BRAVE MÄDCHEN TUN DAS NICHT");
+        dieSchoenstenJahreMeinesLebens.add("DIE SCHÖNSTEN JAHRE MEINES LEBENS");
+        iStillBelieve.add("I STILL BELIEVE");
+        jeanSerbegAgainstAllEnemies.add("JEAN SEBREG - AGAINST ALL ENEMIES");
+        justMercy.add("JUST MERCY");
     }
     public void showMovies(){
 
